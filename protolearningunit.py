c2cf10360ff61b8f6c1d4e53751b012c6df43321
Rémi Emonet <remi.emonet@reverse--com.heeere>
2014-07-23 12:48:31 +0200

    "edit" command to remove the bower dep and clone the repo in its place (no explicit fork yet) (no reverse operation yet)

--- protolearningunit.py
+++ protolearningunit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 
+import subprocess
 from subprocess import call
 import sys
 import json
@@ -17,6 +18,43 @@ def generateIgnoreFile():
                 with open(".gitignore", "a") as ignoreFile:
                     ignoreFile.write(name+"/\n")
 
+import os
+import os.path
+def moveAway(f):
+    assert os.path.exists(f)
+    to = f+".old"
+    if os.path.exists(to):
+        moveAway(to)
+    os.rename(f, to)
+
+def bowerJson():
+    with open("bower.json") as f:
+        return json.load(f)
+
+def bowerList():
+    p = subprocess.Popen(['bower', 'list', '-json'], stdout=subprocess.PIPE)
+    output = p.communicate()[0]
+    return json.loads(output)
+
+def startEditing(args):
+    assert len(args) == 1
+    what = args[0]
+    ok = False
+    bJson = bowerJson()
+    if not what in bJson["dependencies"]:
+        print(what+" is not a dependency (according to bower.json)")
+        return
+    name = what
+    bList = bowerList()
+    url = bList["dependencies"][what]["pkgMeta"]["_source"]
+    del bJson["dependencies"][what]
+    with open("bower.json", "w") as fo:
+        json.dump(bJson, fo, indent=2, sort_keys=True)
+    moveAway(what)
+    call(["git", "clone", url, name])
+
+### 
+
 if len(sys.argv) == 1:
     print("USAGE: TODO")
     exit(0)
@@ -26,5 +64,7 @@ if c == "build":
     call("jekyll build --plugins template/_plugins --layouts template/_layouts --config template/_config.yml".split(" "))
 elif c == "ignore":
     generateIgnoreFile()
+elif c == "edit":
+    startEditing(sys.argv[2:])
 else:
     call(["bower"] + sys.argv[1:])
